f22202be64911d283e959720b8bac315605cdc74
castaval <alexander.butusov.03@mail.ru>
2023-04-24 19:28:02 +0300

    Shoes module

--- src/shoes/shoes.module.ts
+++ src/shoes/shoes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ShoesService } from './shoes.service';
 import { ShoesController } from './shoes.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ShoesController],
   providers: [ShoesService]
 })
