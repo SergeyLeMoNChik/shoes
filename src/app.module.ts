e46dbca4034378e4de79b806f98a6abc3481e5c5
castaval <alexander.butusov.03@mail.ru>
2023-04-26 01:37:45 +0300

    Add sale

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ShoesModule } from './shoes/shoes.module';
 import { DirectorModule } from './director/director.module';
 import { BuyerModule } from './buyer/buyer.module';
 import { SallerModule } from './saller/saller.module';
+import { SaleModule } from './sale/sale.module';
 
 @Module({
-  imports: [ShoesModule, DirectorModule, BuyerModule, SallerModule],
+  imports: [ShoesModule, DirectorModule, BuyerModule, SallerModule, SaleModule],
   controllers: [AppController],
   providers: [AppService],
 })
